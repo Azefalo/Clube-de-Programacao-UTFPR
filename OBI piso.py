f1495d8a4f7eae869089a865bb50818aad23c12d
João Gabriel Buttow <65840609+Azefalo@users.noreply.github.com>
2021-04-12 02:11:22 -0300

    Add files via upload

---
+++ OBI piso.py
@@ -0,0 +1,7 @@
+# https://olimpiada.ic.unicamp.br/pratique/p2/2018/f1/piso/
+L = int(input())
+C = int(input())
+tipo1 = (L * C) + ((L - 1 ) * (C - 1))
+tipo2 = (L - 1) * 2 + (C - 1) * 2
+print(tipo1)
+print(tipo2)
