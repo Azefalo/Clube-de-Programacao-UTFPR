d4e378df5d61384a9051707a108d3429365d23e0
João Gabriel Buttow Albuquerque <65840609+Azefalo@users.noreply.github.com>
2021-04-23 17:55:21 -0300

    Add files via upload

---
+++ URI 1095.py
@@ -0,0 +1,7 @@
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1095
+I = 1
+J = 60
+for i in range(13): 
+    print(f"I={I} J={J}")
+    I += 3
+    J -= 5
---
+++ URI 1096.py
@@ -0,0 +1,7 @@
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1096
+I = 1
+J = 7
+for i in range(5):
+    for i in range(3):
+        print(f"I={I} J={J-i}")
+    I += 2
